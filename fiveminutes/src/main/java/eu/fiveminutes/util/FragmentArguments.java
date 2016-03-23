b4c7316dc973fd02b2df6b8ae8b3d81dec0934ff
Luka <luka.babic2506@gmail.com>
2016-03-23 09:20:39 +0100

    Adding 'final' modifier to FragmentArguments method params.

--- fiveminutes/src/main/java/eu/fiveminutes/util/FragmentArguments.java
+++ fiveminutes/src/main/java/eu/fiveminutes/util/FragmentArguments.java
@@ -12,45 +12,46 @@ public final class FragmentArguments {
 
     }
 
-    public static void putString(Fragment fragment, String key, String value) {
-        Bundle args = getArgumentsOrDefaultBundle(fragment);
+    public static void putString(final Fragment fragment, final String key, final String value) {
+        final Bundle args = getArgumentsOrDefaultBundle(fragment);
         args.putString(key, value);
         fragment.setArguments(args);
     }
 
-    public static void putBoolean(Fragment fragment, String key, boolean value) {
-        Bundle args = getArgumentsOrDefaultBundle(fragment);
+    public static void putBoolean(final Fragment fragment, final String key, final boolean value) {
+        final Bundle args = getArgumentsOrDefaultBundle(fragment);
         args.putBoolean(key, value);
         fragment.setArguments(args);
     }
 
-    public static void putLong(Fragment fragment, String key, long value) {
-        Bundle args = getArgumentsOrDefaultBundle(fragment);
+    public static void putLong(final Fragment fragment, final String key, final long value) {
+        final Bundle args = getArgumentsOrDefaultBundle(fragment);
         args.putLong(key, value);
         fragment.setArguments(args);
     }
 
-
-    public static void putInt(Fragment fragment, String key, int value) {
-        Bundle args = getArgumentsOrDefaultBundle(fragment);
+    public static void putInt(final Fragment fragment, final String key, final int value) {
+        final Bundle args = getArgumentsOrDefaultBundle(fragment);
         args.putInt(key, value);
         fragment.setArguments(args);
     }
 
-    public static void putSerializable(Fragment fragment, String key, Serializable value) {
-        Bundle args = getArgumentsOrDefaultBundle(fragment);
+    public static void putSerializable(final Fragment fragment, final String key, final Serializable value) {
+        final Bundle args = getArgumentsOrDefaultBundle(fragment);
         args.putSerializable(key, value);
         fragment.setArguments(args);
     }
 
-    public static void putParcelable(Fragment fragment, String key, Parcelable value) {
-        Bundle args = getArgumentsOrDefaultBundle(fragment);
+    public static void putParcelable(final Fragment fragment, final String key, final Parcelable value) {
+        final Bundle args = getArgumentsOrDefaultBundle(fragment);
         args.putParcelable(key, value);
         fragment.setArguments(args);
     }
 
-    private static Bundle getArgumentsOrDefaultBundle(Fragment fragment) {
-        return fragment.getArguments() == null ? new Bundle() : fragment.getArguments();
+    private static Bundle getArgumentsOrDefaultBundle(final Fragment fragment) {
+        final Bundle bundle = fragment.getArguments();
+
+        return bundle != null ? bundle : new Bundle();
     }
 }
 
