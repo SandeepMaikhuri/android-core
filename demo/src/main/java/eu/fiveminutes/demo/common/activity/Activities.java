afd0025e78b6ca2fd34b38a34db3cca486f16a9d
Luka <luka.babic2506@gmail.com>
2016-06-05 13:09:22 +0200

    Removing unused activities and upgrading gradle plugin version.

--- demo/src/main/java/eu/fiveminutes/demo/common/activity/Activities.java
+++ demo/src/main/java/eu/fiveminutes/demo/common/activity/Activities.java
@@ -2,14 +2,12 @@ package eu.fiveminutes.demo.common.activity;
 
 import eu.fiveminutes.demo.example.ExampleActivity;
 import eu.fiveminutes.demo.fonts.CustomFontActivity;
-import eu.fiveminutes.demo.threading.activity.ThreadingMainActivity;
 
 public class Activities {
 
     static Class<?>[] classes = {
             ExampleActivity.class,
-            CustomFontActivity.class,
-            ThreadingMainActivity.class
+            CustomFontActivity.class
 
             // Add your demo activity classes here, like this:
             //, MyDemoActivity.class
