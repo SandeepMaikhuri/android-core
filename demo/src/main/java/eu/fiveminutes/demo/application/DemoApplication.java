ed199cf52bf0d227d9ebe8e90cf94485cf413403
Luka <luka.babic2506@gmail.com>
2016-07-17 15:36:32 +0200

    Adding example for Foreground.

---
+++ demo/src/main/java/eu/fiveminutes/demo/application/DemoApplication.java
@@ -0,0 +1,24 @@
+package eu.fiveminutes.demo.application;
+
+import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
+
+import eu.fiveminutes.demo.example.ForegroundListenerImpl;
+import eu.fiveminutes.util.Foreground;
+import eu.fiveminutes.util.ForegroundImpl;
+
+public final class DemoApplication extends Application {
+
+    private Foreground foreground;
+
+    private Foreground.Listener listener;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        foreground = new ForegroundImpl(this, new Handler(Looper.getMainLooper()));
+        listener = new ForegroundListenerImpl();
+        foreground.addListener(listener);
+    }
+}
