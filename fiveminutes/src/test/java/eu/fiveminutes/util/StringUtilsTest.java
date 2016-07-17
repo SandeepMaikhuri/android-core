8db63e5e0f2e0a718dff79bb45f82f79faec069f
Luka <luka.babic2506@gmail.com>
2016-07-17 14:07:48 +0200

    Refactoring StringUtils test.

--- fiveminutes/src/test/java/eu/fiveminutes/util/StringUtilsTest.java
+++ fiveminutes/src/test/java/eu/fiveminutes/util/StringUtilsTest.java
@@ -1,14 +1,31 @@
 package eu.fiveminutes.util;
 
-import android.test.AndroidTestCase;
+import junit.framework.Assert;
 
-public class StringUtilsTest extends AndroidTestCase {
+import org.junit.Before;
+import org.junit.Test;
 
-    public void test() {
-        final StringUtils stringUtils = new StringUtilsImpl();
+public final class StringUtilsTest {
 
-        assertTrue(stringUtils.isStringEmpty(""));
-        assertTrue(stringUtils.isStringEmpty(null));
-        assertTrue(!stringUtils.isStringEmpty("hello!"));
+    private StringUtils stringUtils;
+
+    @Before
+    public void setUp() {
+        stringUtils = new StringUtilsImpl();
+    }
+
+    @Test
+    public void testIsStringEmptyOrNot() {
+        Assert.assertTrue(stringUtils.isStringEmpty(""));
+        Assert.assertTrue(stringUtils.isStringEmpty(null));
+        Assert.assertTrue(!stringUtils.isStringEmpty("hello!"));
+    }
+
+    @Test
+    public void testStringConcatenation() {
+        final String[] values = {"1", "2", "3", "4", "5"};
+        final String result = "1, 2, 3, 4, 5";
+
+        Assert.assertEquals(result, stringUtils.arrayToCsvString(values));
     }
 }
